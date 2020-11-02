e2afecfd080ec4157c9f09907d842ebcec73c812
pawciow <pawciow123@gmail.com>
2020-11-02 13:11:54 +0100

    Dodaj pliki projektów.

---
+++ Controllers/WorkersManagementController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mechanical_Workshop_Management_System.Controllers
+{
+    public class WorkersManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("WorkersManagement");
+        }
+    }
+}
---
+++ Models/Worker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mechanical_Workshop_Management_System.Models
+{
+    public class Worker
+    {
+        public string Name { set; get; }
+        public Specialization Speciality { get; set; }
+        public enum Specialization
+        {
+            Mechanic, // mechanik
+            Car_tinsmith, // blacharz samochodowy
+            car_painter // lakiernik
+        }
+    }
+}
